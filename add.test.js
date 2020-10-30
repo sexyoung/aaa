47d1a2b1c4857302f66ffd60a36d4d874b1171a8
sexyoung <Habuche@gmail.com>
2020-10-30 03:22:38 +0000

    Updates

---
+++ add.test.js
@@ -0,0 +1,9 @@
+// const { it } = require("@jest/globals")
+const { expect } = require('@jest/globals');
+var add = require('./add');
+
+describe('add', () => {
+  test('should add 1+2 = 3', () => {
+    expect(add(1,2)).toBe(3);
+  })
+})
